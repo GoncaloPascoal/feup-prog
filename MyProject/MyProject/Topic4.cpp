8f3f7bbb492a5adfa7f43ccc3a6829c65a450e53
Gonçalo Pascoal <thelegendpaco@gmail.com>
2019-03-16 23:41:03 +0000

    Added exercise 4.6
    Also completed exercise 4.5 and created a folder for the 1st project of the course.

--- MyProject/MyProject/Topic4.cpp
+++ MyProject/MyProject/Topic4.cpp
@@ -7,7 +7,7 @@ void Topic4::selectExercise() {
 	cin >> exercise;
 
 	string compound, name;
-	Fraction f;
+	Fraction f1, f2;
 
 	switch (exercise) {
 		case 1:
@@ -27,11 +27,32 @@ void Topic4::selectExercise() {
 			cout << normalizeName(name) << endl;
 			break;
 		case 5:
-			f = readFrac();
-			f = reduceFrac(f);
-			writeFrac(f);
+			f1 = readFrac();
+			f2 = readFrac();
+			
+			f1 = reduceFrac(f1);
+			f2 = reduceFrac(f2);
+			
+			cout << "F1: ";
+			writeFrac(f1);
+			cout << " | F2: ";
+			writeFrac(f2);
 			cout << endl;
+
+			cout << "SUM: ";
+			writeFrac(addFrac(f1, f2));
+			cout << "\nDIFF: ";
+			writeFrac(subtractFrac(f1, f2));
+			cout << "\nMULT: ";
+			writeFrac(multiplyFrac(f1, f2));
+			cout << "\nDIV: ";
+			writeFrac(divideFrac(f1, f2));
+
+			cout << endl;
+
 			break;
+		case 6:
+			testIntArray();
 	}
 }
 
@@ -151,7 +172,13 @@ void Topic4::writeFrac(Fraction f) {
 }
 
 Fraction Topic4::reduceFrac(Fraction f) {
+	bool negative;
 	int a, b, gcd;
+
+	negative = f.numerator < 0 != f.denominator < 0;
+
+	f.numerator = abs(f.numerator);
+	f.denominator = abs(f.denominator);
 
 	a = f.numerator;
 	b = f.denominator;
@@ -169,8 +196,127 @@ Fraction Topic4::reduceFrac(Fraction f) {
 	else
 		gcd = 1;
 
-	f.numerator /= gcd;
+	if (negative)
+		f.numerator /= -gcd;
+	else
+		f.numerator /= gcd;
 	f.denominator /= gcd;
 
 	return f;
+}
+
+Fraction Topic4::addFrac(Fraction f1, Fraction f2) {
+	Fraction result;
+
+	if (f1.denominator == 0 || f2.denominator == 0) {
+		result.numerator = 0;
+		result.denominator = 0;
+	}
+	else if (f1.numerator == 0)
+		result = f2;
+	else if (f2.numerator == 0)
+		result = f1;
+	else {
+		int a, b, scm; // Smallest common multiple
+
+		a = f1.denominator;
+		b = f2.denominator;
+
+		// Euclid's algorithm
+		while (a != b) {
+			if (a > b)
+				a -= b;
+			else
+				b -= a;
+		}
+		scm = f1.denominator * f2.denominator / a;
+
+		result.numerator = f1.numerator * (scm / f1.denominator) + f2.numerator * (scm / f2.denominator);;
+		result.denominator = scm;
+
+		result = reduceFrac(result);
+	}
+
+	return result;
+}
+
+Fraction Topic4::subtractFrac(Fraction f1, Fraction f2) {
+	f2.numerator = -f2.numerator;
+	
+	return addFrac(f1, f2);
+}
+
+Fraction Topic4::multiplyFrac(Fraction f1, Fraction f2) {
+	Fraction result;
+	
+	result.numerator = f1.numerator * f2.numerator;
+	result.denominator = f1.denominator * f2.denominator;
+
+	result = reduceFrac(result);
+
+	return result;
+}
+
+Fraction Topic4::divideFrac(Fraction f1, Fraction f2) {
+	Fraction result;
+
+	if (f2.numerator == 0) {
+		result.numerator = 0;
+		result.denominator = 0;
+	}
+	else {
+		result.numerator = f1.numerator * f2.denominator;
+		result.denominator = f1.denominator * f2.numerator;
+	}
+
+	result = reduceFrac(result);
+
+	return result;
+}
+
+void Topic4::readIntArray(int a[], size_t nElem) {
+	for (size_t i = 0; i < nElem; i++) {
+		cout << "Integer #" << i << ": ";
+		cin >> a[i];
+	}
+}
+
+size_t Topic4::searchValueInIntArray(const int a[], size_t nElem, int value, int index[]) {
+	int occurrences = 0;
+
+	for (size_t i = 0; i < nElem; i++) {
+		if (a[i] == value) {
+			index[occurrences] = i;
+			occurrences++;
+		}
+	}
+
+	return occurrences;
+}
+
+void Topic4::testIntArray() {
+	const size_t ARRAY_SIZE = 10;
+	int arr[ARRAY_SIZE], index[ARRAY_SIZE];
+	int value;
+	size_t occurrences;
+
+	cout << "Reading integer array...\n";
+	readIntArray(arr, ARRAY_SIZE);
+	
+	cout << "Enter a value: ";
+	cin >> value;
+	occurrences = searchValueInIntArray(arr, ARRAY_SIZE, value, index);
+
+	if (occurrences == 0)
+		cout << "Value not found in the array.\n";
+	else {
+		cout << "The value appears " << occurrences << " time(s) in the array.\n";
+		cout << "Indexes of occurrences: ";
+		for (size_t i = 0; i < occurrences; i++) {
+			cout << index[i];
+			if (i < occurrences - 1)
+				cout << ", ";
+		}
+		cout << endl;
+	}
 }
--- MyProject/MyProject/Topic4.h
+++ MyProject/MyProject/Topic4.h
@@ -24,4 +24,12 @@ private:
 	Fraction readFrac();
 	void writeFrac(Fraction f);
 	Fraction reduceFrac(Fraction f);
+	Fraction addFrac(Fraction f1, Fraction f2);
+	Fraction subtractFrac(Fraction f1, Fraction f2);
+	Fraction multiplyFrac(Fraction f1, Fraction f2);
+	Fraction divideFrac(Fraction f1, Fraction f2);
+
+	void readIntArray(int a[], size_t nElem);
+	size_t searchValueInIntArray(const int a[], size_t nElem, int value, int index[]);
+	void testIntArray();
 };
